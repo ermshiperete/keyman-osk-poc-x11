19ea2c9e63f172e956814548e5bfd3a7ef665bd1
Eberhard Beilharz <eb1@sil.org>
2023-01-18 16:33:26 +0100

    Use new Keyman DBus method to send junk of text

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+import json
 import logging
+import dbus
+import dbus.bus
+import dbus.mainloop.glib
+import dbus.service
 import gi
-import time
+import os
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
@@ -10,9 +15,13 @@ gi.require_version('WebKit2', '4.0')
 
 from Xlib import X, XK, protocol
 from Xlib.display import Display
-from gi.repository import Gdk, GLib, Gtk, WebKit2
+from gi.repository import GLib, Gtk, WebKit2
 
 class MainWindow(Gtk.Window):
+    KM_DBUS_NAME = "com.Keyman"
+    KM_DBUS_PATH  = "/com/Keyman/IBus"
+    KM_DBUS_IFACE = "com.Keyman"
+
     def __init__(self) -> None:
         args = {
             "skip_taskbar_hint": True,
@@ -30,10 +39,28 @@ class MainWindow(Gtk.Window):
         self.display = Display()
         self.screen = self.display.screen()
         self.root = self.screen.root
-        self._homeUrl = GLib.filename_to_uri(
-            "/home/eberhard/Projects/TrashBin/keyman-osk-poc-x11/test.html", None)
+        currentDir = os.path.dirname(os.path.realpath(__file__))
+        self._homeUrl = GLib.filename_to_uri(os.path.join(currentDir, 'test.html'), None)
         self._onStartup()
         self._onActivate()
+
+        # connect to session bus
+        # Use D-bus main loop by default
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        try:
+            self._bus = dbus.SessionBus()
+        except dbus.exceptions.DBusException:
+            raise RuntimeError("D-Bus session bus unavailable")
+        self._bus.add_signal_receiver(self._on_name_owner_changed,
+                                      "NameOwnerChanged",
+                                      dbus.BUS_DAEMON_IFACE,
+                                      arg0=self.KM_DBUS_NAME)
+        # Initial state
+        proxy = self._bus.get_object(
+            dbus.BUS_DAEMON_NAME, dbus.BUS_DAEMON_PATH)
+        result = proxy.NameHasOwner(
+            self.KM_DBUS_NAME, dbus_interface=dbus.BUS_DAEMON_IFACE)
+        self._set_connection(bool(result))
 
     def run(self) -> None:
         self.resize(576, 324)
@@ -87,10 +114,6 @@ class MainWindow(Gtk.Window):
         gdkWindow = self.get_window()
         # This works on X11
         gdkWindow.set_accept_focus(False)
-        # self._webView.set_focus_on_click(False)
-        # self.set_focus_on_click(False)
-        # self.set_can_focus(False)
-        # self.set_focus_on_map(False)
 
     def _onConnect(self, webview, arg) -> None:
         self.set_title(webview.get_title())
@@ -101,8 +124,14 @@ class MainWindow(Gtk.Window):
         return True
 
     def _button_clicked(self, manager, jsResult) -> None:
+        val = jsResult.get_js_value()
         logging.info('Button Clicked: ' + jsResult.get_js_value().to_string())
-        self._send_key(jsResult.get_js_value().to_string()[0])
+        if val.is_string():
+            self._send_key(jsResult.get_js_value().to_string()[0])
+        else:
+            object = json.loads(val.to_json(0))
+            assert('text' in object)
+            self._send_text(object['text'])
 
     def _send_key(self, emulated_key):
         shift_mask = 0  # or Xlib.X.ShiftMask
@@ -131,6 +160,29 @@ class MainWindow(Gtk.Window):
         window.send_event(event, propagate=True)
         self.display.sync()
 
+    def _send_text(self, text):
+        if self.proxy:
+            print(text)
+            self.proxy.SendText(text, dbus_interface=self.KM_DBUS_IFACE)
+        else:
+            print('Keyman not active')
+
+    def _on_name_owner_changed(self, name, old, new):
+        '''
+        The daemon has de/registered the name.
+        Called when ibus-keyman un/loads a keyboard
+        '''
+        active = old == ""
+        self._set_connection(active)
+
+    def _set_connection(self, active):
+        '''
+        Update interface object, state and notify listeners
+        '''
+        if active:
+            self.proxy = self._bus.get_object(self.KM_DBUS_NAME, self.KM_DBUS_PATH)
+        else:
+            self.proxy = None
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
